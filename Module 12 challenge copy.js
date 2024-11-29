943ef56a37bad41ce5fc480824bc16f50f047d75
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-11-28 18:45:48 -0600

    Add files via upload

---
+++ Module 12 challenge copy.js
@@ -0,0 +1,115 @@
+import React from 'react';
+
+
+import Navigation from '.Navigation';
+
+/// Header component
+
+const Header = () => {
+
+// footer component
+    
+function createFooter() {
+    const footer = document.createElement('footer');
+    footer.innerHTML =
+
+    document.body.appendChild(footer);
+}
+
+createFooter();
+
+
+const Footer = () => {
+    
+        
+
+    
+    
+};
+        
+
+    
+
+
+  
+        
+
+    
+
+
+
+        
+    
+
+
+
+    
+    
+
+
+
+//// Navigation component
+
+
+
+
+
+    
+       
+
+        
+    
+
+       
+
+    
+
+        
+    
+
+   
+   
+
+        
+   
+   
+
+
+
+   
+   
+
+    
+       
+   
+
+
+
+    
+
+        
+   
+   
+    
+
+
+        
+
+   
+
+    
+    
+    
+    
+
+
+       
+
+
+            
+
+    
+    
+    
+    
+}
